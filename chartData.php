744ee4847818300a2c12e91653808f0c221dd3d3
Rob <rnice01@aurora.edu>
2015-04-15 23:02:02 -0500

    Update chartData.php

--- chartData.php
+++ chartData.php
@@ -1,8 +1,5 @@
 
 
-
-
-
 <?php  
 
  
@@ -12,62 +9,36 @@ $INFILES['feb'] ='022015data.csv';
 $INFILES['march'] ='032015data.csv';     
 $INFILES['april'] ='042015data.csv';     
 $INFILES['may'] ='052015data.csv';
-$month = 'feb';
 //$month = $_REQUEST['monthData'];
-
+$month = 'feb';
 $f = $INFILES[$month];
 error_reporting(E_ERROR | E_WARNING | E_PARSE);
 csv_to_json($HOME, $f);     
 $ALL_DATA = array();
+
 $cumm = null;
 $cumm_count = null;
 $averageTemp = null;
-
 $max = 0;
-$min = 5834975;
+
 
                 function csv_to_json($home, $file){
-                  global $cumm, $cumm_count, $averageTemp, $min, $max;
+                  global $cumm, $cumm_count, $averageTemp;
                     $FN ="$home/$file";              
                     $DATA = @file( $FN );              
                         if ( !empty( $DATA) )             
                      foreach ( $DATA as $ct => $L ) {                  
                             $ALL_DATA[] = $L;
 
-                        
-
                             foreach($ALL_DATA as $index =>$val){
+                                
                                
                           list($date, $hr, $temp, $b, $a) = split(",", $val);
-
                           
-                          
-                                if(isset($_GET[$date])){
+                                  $temps[$date][$hr] = $temp;
                                   $cumm[$date] += $temp;
                                   $cumm_count[$date] += 1;
-                                }
-                                else if(isset($_GET[$temp])){
-                                  $cumm[$date] += $temp;
-                                  $cumm_count[$date] += 1;
-                                }
-                                else{
-                                   $cumm[$date] += $temp;
-                                  $cumm_count[$date] += 1;
-                                }
-
-                                if ($temp > $max)
-                                  { $max = $temp; }
-
-                                $maxTemp[$date] = $max; 
-                                echo "date =" . $date ."max=" . $max;
-
-                  
-
-                                if ($temp < $min)
-                                  { $min = $temp; }
-
-                                $minTemp[$date] = $min; 
-                                echo "date =" . $date . "min=" . $min;
+                              
                               
                           
                           
@@ -79,7 +50,13 @@ $min = 5834975;
                             
                             }               
                       }
-                      
+                     
+                      foreach($temps as $date=>$hr){
+                        print"The high for ".$date.": " .intval(max($hr))."</br>";
+                        print "The low for ".$date.": ".intval(min($hr))."</br>";
+                       
+                      }
+                     
                      
                      $file = fopen("jsonData.php" , "w+");
                      
